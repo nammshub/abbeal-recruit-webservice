d520423be20329cd6afabedd4fb1fafe8f8226df
nammshub <vincent.dhainaut@gmail.com>
2019-03-07 18:01:04 +0100

    get questions fields in controller

--- src/main/java/com/abbeal/recruitwebservice/controllers/QuestionController.java
+++ src/main/java/com/abbeal/recruitwebservice/controllers/QuestionController.java
@@ -19,37 +19,46 @@ import com.abbeal.recruitwebservice.services.QuestionService;
 
 @RestController
 public class QuestionController {
-	
+
 	@Autowired
 	QuestionService questionService;
-	
+
 	@Autowired
 	ModelMapper modelMapper;
 
 	@GetMapping("/questions")
 	public Set<QuestionDto> findAllQuestion() {
 		Set<Question> questions = questionService.findAll();
-		return questions.stream().map(this::convertToDto)
-        .collect(Collectors.toSet());
+		return questions.stream().map(this::convertToDto).collect(Collectors.toSet());
 	}
-	
+
 	@GetMapping("/questions/{field}")
 	public Set<QuestionDto> findAllQuestionByField(@PathVariable String field) {
-		Set<Question> questions =questionService.findAllByField(field);
-		return questions.stream().map(this::convertToDto)
-		        .collect(Collectors.toSet());
+		Set<Question> questions = questionService.findAllByField(field);
+		return questions.stream().map(this::convertToDto).collect(Collectors.toSet());
 	}
-	
+
+	/**
+	 * 
+	 * @return all the distinct fields of the available questions
+	 */
+	@GetMapping("/questions/fields")
+	public Set<String> findAllQuestionFields() {
+		Set<Question> questions = questionService.findAll();
+		return questions.stream().map(this::convertToDto).map(QuestionDto::getField).distinct().sorted()
+				.collect(Collectors.toSet());
+	}
+
 	@PostMapping("/questions")
 	public ResponseEntity<HttpStatus> createQuestion(@RequestBody QuestionDto question) {
 		questionService.save(convertToEntity(question));
 		return ResponseEntity.ok(HttpStatus.OK);
 	}
-	
+
 	private QuestionDto convertToDto(Question q) {
 		return modelMapper.map(q, QuestionDto.class);
 	}
-	
+
 	private Question convertToEntity(QuestionDto q) {
 		return modelMapper.map(q, Question.class);
 	}
