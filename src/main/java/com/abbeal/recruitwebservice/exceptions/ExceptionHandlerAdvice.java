a26f4bc53b8b22e7d75fd80674a41c3a444a9d12
nammshub <vincent.dhainaut@gmail.com>
2019-03-08 17:39:59 +0100

    ExceptionHandlerAdvice

---
+++ src/main/java/com/abbeal/recruitwebservice/exceptions/ExceptionHandlerAdvice.java
@@ -0,0 +1,24 @@
+package com.abbeal.recruitwebservice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(QuizzNotPresentException.class)
+    public ResponseEntity<String> handleException(QuizzNotPresentException e) {
+        return ResponseEntity
+                .status(HttpStatus.EXPECTATION_FAILED)
+                .body("Le quizz demandé n'existe pas !");
+    }        
+    
+    @ExceptionHandler(NotEnoughQuestionsException.class)
+    public ResponseEntity<String> handleException(NotEnoughQuestionsException e) {
+        return ResponseEntity
+                .status(HttpStatus.EXPECTATION_FAILED)
+                .body("Il n'y a pas assez de questions pour satisfaire la demande !");
+    }    
+}
