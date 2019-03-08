07e34b7e5f6e23c6808f17bda49ea7be0b0a3f68
nammshub <vincent.dhainaut@gmail.com>
2019-03-08 17:38:21 +0100

    CORS: tolerate PATCH

--- src/main/java/com/abbeal/recruitwebservice/SimpleCORSFilter.java
+++ src/main/java/com/abbeal/recruitwebservice/SimpleCORSFilter.java
@@ -9,18 +9,23 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleCORSFilter implements Filter {
+	Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
 
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        response.setHeader("Content-Type", "application/json");
-        chain.doFilter(req, res);
-    }
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,  Content-Type");
+		response.setHeader("Access-Control-Expose-Headers", "Authorization");
+		response.setHeader("Content-Type", "application/json");
+		chain.doFilter(req, res);
+	}
 }
